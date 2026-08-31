f7f678538900e436778f90bbefb1c9c3a93657a1
surhaliiznik <ceribasbso6716@gmail.com>
2026-08-31 18:16:03 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://lhltolrtgnfkbwfkpaex.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_xdWMVRunvPSeiMw2vfGWyw_l6dTnBsn";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHRvbHJ0Z25ma2J3ZmtwYWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTExNjYsImV4cCI6MjEwMTY4NzE2Nn0.y8OryUG7jK2lvDwKD6Y61oqPJnzKzd9RWohRQc1bBgw";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
