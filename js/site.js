1e11a453d7a7732d2c71c326ad1adc6c44f42263
surhaliiznik <ceribasbso6716@gmail.com>
2026-08-31 18:10:13 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 /* SUR HALI - TEMİZ SİSTEM (v101.0) */
 
 var SUR_SUPABASE_URL = "https://lhltolrtgnfkbwfkpaex.supabase.co";
-var SUR_SUPABASE_KEY = "sb_publishable_xdWMVRunvPSeiMw2vfGWyw_l6dTnBsn";
+var SUR_SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxobHRvbHJ0Z25ma2J3ZmtwYWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMTExNjYsImV4cCI6MjEwMTY4NzE2Nn0.y8OryUG7jK2lvDwKD6Y61oqPJnzKzd9RWohRQc1bBgw";
 
 var GROQ_K1 = "gsk_1XmszSHMd9GCOKVsfN44WGdyb3";
 var GROQ_K2 = "FYIa5eKHxX5TchnxdWZvVQJZP5";
