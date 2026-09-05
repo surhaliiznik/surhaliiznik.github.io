bd1743efcfac0c29c70002b2a50f6f88e6a7cc77
surhaliiznik <ceribasbso6716@gmail.com>
2026-09-05 17:50:21 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,3 @@
-```javascript
 /* ==========================================================
    SUR HALI İZNİK
    ANA SITE JAVASCRIPT
@@ -3289,4 +3288,4 @@ if (
 
     initializeSurHaliSite();
 }
-```
+
